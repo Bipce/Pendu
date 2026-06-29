b4e02b532a3ccd394a6823a4c0c6c3ced1727a6a
Claire Royer <claireroyer57@gmail.com>
2026-06-29 23:02:08 +0200

    feat(back): handle player connection with username

--- backend/src/hangman/hangman.gateway.ts
+++ backend/src/hangman/hangman.gateway.ts
@@ -1,11 +1,27 @@
-import { SubscribeMessage, WebSocketGateway } from "@nestjs/websockets";
-import { Socket } from "socket.io";
+import { OnGatewayConnection, OnGatewayDisconnect, WebSocketGateway } from "@nestjs/websockets";
+import { DefaultEventsMap, Socket } from "socket.io";
+
+interface HangmanSocketData {
+  username: string;
+}
+
+type HangmanSocket = Socket<DefaultEventsMap, DefaultEventsMap, DefaultEventsMap, HangmanSocketData>;
 
 @WebSocketGateway({ cors: { origin: "http://localhost:5173" } })
-export class HangmanGateway {
-  @SubscribeMessage("ping")
-  handlePingPong(client: Socket): void {
-    console.log("ping received by: ", client.id);
-    client.emit("pong");
+export class HangmanGateway implements OnGatewayConnection, OnGatewayDisconnect {
+  handleConnection(client: HangmanSocket) {
+    const username: unknown = client.handshake.auth.username;
+
+    if (typeof username !== "string" || username.trim() === "") {
+      client.disconnect();
+      return;
+    }
+
+    client.data.username = username;
+    console.log(`${username} connected (${client.id})`);
+  }
+
+  handleDisconnect(client: HangmanSocket) {
+    if (client.data.username) console.log("Disconnect: ", client.data.username);
   }
 }
