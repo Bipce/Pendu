29207b71caf5cadf05b65ff6d9b7db61ff4ae736
Claire Royer <claireroyer57@gmail.com>
2026-06-29 20:41:47 +0200

    feat(front): connect to websocket server

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,18 @@
+import { io } from "socket.io-client";
+import { useEffect } from "react";
+
 function App() {
+  useEffect(() => {
+    const socket = io("http://localhost:3000");
+
+    socket.on("connect", () => socket.emit("ping"));
+    socket.on("pong", () => console.log(socket.id));
+
+    return () => {
+      socket.disconnect();
+    };
+  }, []);
+
   return <h1>Pendu</h1>;
 }
 
