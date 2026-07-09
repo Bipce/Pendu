3aedd5c270f4b1e5280871e01af7f66781c6af56
Claire Royer <claireroyer57@gmail.com>
2026-07-09 16:25:59 +0200

    feat(front): sync room state on room_updated

--- frontend/src/socket/bindEvents.ts
+++ frontend/src/socket/bindEvents.ts
@@ -3,3 +3,5 @@ import { useHangmanStore } from "../store/hangmanStore.ts";
 
 socket.on("connect", () => useHangmanStore.setState({ isConnected: true }));
 socket.on("disconnect", () => useHangmanStore.setState({ isConnected: false }));
+
+socket.on("room_updated", room => useHangmanStore.setState({ room }));
