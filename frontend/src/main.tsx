acbd2441600b06519a22a90f7e8936917761e3ee
Claire Royer <claireroyer57@gmail.com>
2026-07-10 15:48:20 +0200

    chore(front): add Roboto and Titan One fonts

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,6 +3,11 @@ import { BrowserRouter } from "react-router";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./socket/bindEvents.ts";
+
+import "@fontsource/titan-one";
+import "@fontsource/roboto/300-italic.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/700.css";
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
